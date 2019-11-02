b39c850749f7071d1094f0631867e2c3e254a97f
redtomroberts <55162586+redtomroberts@users.noreply.github.com>
2019-11-02 09:52:53 -0400

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+<script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-analytics.js"></script>
+
+<script>
+    
+  var firebaseConfig = {
+    apiKey: "AIzaSyB6nuz_M326fOPxo7-aWQfCMAqkxs3foqY",
+    authDomain: "elder-ec37d.firebaseapp.com",
+    databaseURL: "https://elder-ec37d.firebaseio.com",
+    projectId: "elder-ec37d",
+    storageBucket: "elder-ec37d.appspot.com",
+    messagingSenderId: "657890035204",
+    appId: "1:657890035204:web:a8eefca192ae4225e65751",
+    measurementId: "G-KS02S1HB4G"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
