eedfb7d0bfc2755657c7148c2df6471fd8b622d7
David Hauck <dhauck@mac.com>
2019-11-06 13:37:57 -0500

    added filter to map table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,21 +6,103 @@ let phone;
 let latitude;
 let longitude;
 let address;
-let city;;
+let city = "Boston";
+let keywords = "Italian";
 let state;
 let zipcode;
 let name;
+let center_long = 0;
+let center_lat = 0;
+let yelp = true;
+
+let startDate = moment().format('YYYY-MM-DD');
+console.log(startDate);
+startDate = startDate.replace(/-/g, "");
+console.log(startDate);
+
+// $("#start-date").hide();
+
+// $("#category-list").change(function(){
+//     $("#start-date").hide();
+//     let selection = $(this).val();
+//     if (selection === "events"){
+//       $("#start-date").show();
+//     } 
+//      else {
+//         $("#start-date").hide(); 
+//      }
+//   });
 
 $(".user-selection").on("click", function (e) {
+    center_long = 0;
+    center_lat = 0;
     $("#listings").html(" ");
-    // $(".map").clear();
     e.preventDefault();
-    let newEvent = $(this).val();
-    firstLoad(newEvent);
+    newEvent = $(this).val();
+    if (newEvent === "entertainment") {
+        newEvent = "art";
+        eventfulSelected(newEvent);
+    }
+    else {
+        yelpSelected(newEvent);
+    }
 
 });
 
-function firstLoad(newEvent) {
+$("#submit-filter").on("click", function (e) {
+    $("#listings").html(" ");
+    e.preventDefault();
+    city = $("#city-list").val();
+    newEvent = $("#category-list").val();
+    startDate = $("#start-date").val();
+    startDate = startDate.replace(/-/g, "");
+    keywords = $("#keywords").val();
+
+    console.log("new event" + newEvent)
+    let apiEventful = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=fQR9v8ZPXs3sbfJH&location=Boston&category=" + newEvent + "&date=" + startDate + "&keywords=" + keywords;
+
+  $.ajax({
+    url: apiEventful,
+    method: "GET",
+    headers: {
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin": "*",
+        "Authorization": "Bearer VqZqgUX2qboPwa-PUY_ZeLscFpp23iDTB7vFF4T6PKTfjFXNSOM8hpsoJmdnKeMGbXiYPmAuZLzGNVEkuUImrrICCSouTCpoALIBX7gaYV8-vDj_OvlCAbWpKsLAXXYx"
+    },
+}).then(function (response) {
+    console.log(response);
+    response = JSON.parse(response);
+    console.log(response);
+    eventfulList(response)
+});
+
+});
+
+
+function eventfulSelected(newEvent) {
+    console.log("new event" + newEvent)
+    let apiEventful = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=fQR9v8ZPXs3sbfJH&location=" + city + "&category=" + newEvent + "&date=" + startDate + "&keywords=" + keywords;
+
+  $.ajax({
+    url: apiEventful,
+    method: "GET",
+    headers: {
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin": "*",
+        "Authorization": "Bearer VqZqgUX2qboPwa-PUY_ZeLscFpp23iDTB7vFF4T6PKTfjFXNSOM8hpsoJmdnKeMGbXiYPmAuZLzGNVEkuUImrrICCSouTCpoALIBX7gaYV8-vDj_OvlCAbWpKsLAXXYx"
+    },
+}).then(function (response) {
+    // console.log(response);
+    response = JSON.parse(response);
+    // console.log(response);
+    eventfulList(response)
+});
+
+}
+
+function yelpSelected(newEvent) {
     console.log("new event" + newEvent)
     // TA questions cors-anywhere.herokuapp.com
     let apiYelp = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + newEvent + "&location=Boston";
@@ -37,34 +119,38 @@ function firstLoad(newEvent) {
 }).then(function (response) {
     console.log(response);
 
-    loadMap(response)
+    yelpList(response)
 });
 
 }
 
-function loadMap(e) {
-    let results = e.total
-    let item = e.businesses;
 
-    if (results > 0) {
+function yelpList(data) {
+
+
+    let results = data.total
+    let item = data.businesses;
+
 
       for (let i = 0; i<7; i++) {
 
             phone = item[i].display_phone;
-            console.log(phone);
             name = item[i].name;
-            console.log(name);
             latitude = item[i].coordinates.latitude;
-            console.log(latitude);
             longitude = item[i].coordinates.longitude;
-            console.log(longitude);
             address = item[i].location.address1;
-            console.log(address);
             city = item[i].location.city;
-            console.log(city);
             state = item[i].location.state;
-            console.log(state);
             zipcode = item[i].location.zip_code;
+            center_long = parseFloat(center_long);
+            center_lat = parseFloat(center_lat);
+            center_long += parseFloat(longitude);
+            center_lat += parseFloat(latitude);
+            // console.log("long " + longitude);
+            // console.log("centerlong " + center_long);
+            // console.log("lat " + latitude);
+            // console.log("centerlat " + center_lat);
+
 
               storesDyn[i] = {
                     "type": "Feature",
@@ -89,16 +175,88 @@ function loadMap(e) {
                   }
             };
 
+            center_long = center_long/7;
+            center_lat = center_lat/7;
+            // console.log("center long " + center_long);
+            // console.log("center lat " + center_lat);
+
           let newStoresList = {
           "type": "FeaturedCollection",
-          "features": storesDyn
+          "features": storesDyn,
+          "center_long": center_long,
+          "center_lat": center_lat
         };
         console.log("*********************");
         console.log(newStoresList);
         storesPass(newStoresList);
         console.log("*********************");
+    
     }
 
+    function eventfulList(data) {
+
+        let item = data.events;
+        center_long = 0;
+        center_lat = 0;
+
+        for (let i = 0; i<7; i++) {
+
+            phone = "<a href=" + item.event[i].venue_url + ">Website</a>";
+            name = item.event[i].title;
+            latitude = item.event[i].latitude;
+            longitude = item.event[i].longitude;
+            address = item.event[i].venue_address;
+            city = item.event[i].city_name;
+            state = item.event[i].region_name;
+            zipcode = item.event[i].postal_code;
+            center_long = parseFloat(center_long);
+            center_lat = parseFloat(center_lat);
+            center_long += parseFloat(longitude);
+            center_lat += parseFloat(latitude);
+            console.log("long " + longitude);
+            console.log("lat " + latitude);
+
+              storesDyn[i] = {
+                    "type": "Feature",
+                    "geometry": {
+                      "type": "Point",
+                      "coordinates": [
+                        longitude,
+                        latitude,
+                      ]
+                    },
+                    "properties": {
+                      "name": name,
+                      "phoneFormatted": phone,
+                      "phone": phone,
+                      "address": address,
+                      "city": city,
+                      "country": "United States",
+                      "crossStreet": " ",
+                      "postalCode": zipcode,
+                      "state": state
+                    }
+                  }
+            };
+            center_long = center_long/7;
+            center_lat = center_lat/7;
+            console.log("center long " + center_long);
+            console.log("center lat " + center_lat);
+
+
+          let newStoresList = {
+          "type": "FeaturedCollection",
+          "features": storesDyn,
+          "center_long": center_long,
+          "center_lat": center_lat
+        };
+        console.log("*********************");
+        console.log(newStoresList);
+        storesPass(newStoresList);
+        console.log("*********************");
+
+    
+    }
 
 
 function storesPass(newStoresList) {
@@ -116,7 +274,11 @@ console.log("....................");
   }
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWRzaGF1Y2siLCJhIjoiY2syZjU4eWFjMGdvbjNvbW8wN3NjZTJxaSJ9.ZHwNa6cWqC0ZdkAKX6YYCQ';
-
+  
+  center_lat = center_lat.toString();
+  center_long = center_long.toString();
+  console.log("center lat " + center_lat);
+  console.log("center long " + center_long);
   // This adds the map
   let map = new mapboxgl.Map({
     // container id specified in the HTML
@@ -124,7 +286,8 @@ console.log("....................");
     // style URL
     style: 'mapbox://styles/mapbox/light-v10',
     // initial position in [long, lat] format
-    center: [-71.0589, 42.3601],
+    // center: [-71.0589, 42.3601],
+    center: [center_long, center_lat],
     // initial zoom
     zoom: 11,
     scrollZoom: true
@@ -263,8 +426,8 @@ stores = newStoresList;
   }
 };
 
-}
 
-firstLoad(newEvent);
+
+yelpSelected(newEvent);
 
 });
